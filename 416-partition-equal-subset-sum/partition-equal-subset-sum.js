d06a79e6eb97ecf623135189b4ac7246da012eca
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-04-18 01:01:57 +0530

    Time: 351 ms (14.02%) | Memory: 89.3 MB (20.46%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -1,16 +1,42 @@
+// var canPartition = function(nums) {
+//     const total = nums.reduce((a, b) => a + b, 0);
+//     if (total % 2 !== 0) return false;
+
+//     const target = total / 2;
+//     const dp = new Array(target + 1).fill(false);
+//     dp[0] = true;
+
+//     for (let num of nums) {
+//         for (let i = target; i >= num; i--) {
+//             dp[i] = dp[i] || dp[i - num];
+//         }
+//     }
+
+//     return dp[target];
+// };
+
 var canPartition = function(nums) {
     const total = nums.reduce((a, b) => a + b, 0);
     if (total % 2 !== 0) return false;
 
     const target = total / 2;
-    const dp = new Array(target + 1).fill(false);
-    dp[0] = true;
 
-    for (let num of nums) {
-        for (let i = target; i >= num; i--) {
-            dp[i] = dp[i] || dp[i - num];
-        }
-    }
+    // Initialize 2D memo array: [nums.length][target + 1]
+    const memo = Array.from({ length: nums.length }, () => Array(target + 1).fill(-1));
 
-    return dp[target];
+    const dfs = (i, currSum) => {
+        if (currSum > target) return false;
+        if (currSum === target) return true;
+        if (i >= nums.length) return false;
+
+        if (memo[i][currSum] !== -1) return memo[i][currSum];
+
+        const take = dfs(i + 1, currSum + nums[i]);
+        const skip = dfs(i + 1, currSum);
+
+        memo[i][currSum] = take || skip;
+        return memo[i][currSum];
+    };
+
+    return dfs(0, 0);
 };
