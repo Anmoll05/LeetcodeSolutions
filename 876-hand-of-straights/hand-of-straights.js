1624671dc71cad2d90aa46485c3ac408f8c739a1
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-05-25 19:47:11 +0530

    Time: 247 ms (9.21%) | Memory: 68.1 MB (30.96%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} hand
+ * @param {number} groupSize
+ * @return {boolean}
+ */
+var isNStraightHand = function(nums, k) {
+    if (nums.length % k !== 0) return false;
+    const heap = new MinPriorityQueue();
+    nums.forEach((e) => {
+        heap.enqueue(e);
+    });
+    while (heap.size()) {
+        let i = 0;
+        let q = [];
+        let lastScanned = null;
+        while (i < k) {
+            if (heap.size()) {
+                let n = heap.dequeue();
+                if (n == lastScanned) {
+                    q.push(n);
+                } else if (lastScanned !== null && n !== lastScanned + 1) {
+                    return false;
+                }
+                else {
+                    lastScanned = n;
+                    i++;
+                    console.log('ls', lastScanned)
+                }
+            } else {
+                return false;
+            }
+        };
+        console.log(q)
+        while (q.length) {
+            heap.enqueue(q.pop());
+        };
+    }
+    return true;
+};
