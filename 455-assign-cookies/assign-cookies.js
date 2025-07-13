9ff77706b2c2614136f57a15a6ce50b47818d4ed
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-07-13 20:57:48 +0530

    Time: 24 ms (86.01%) | Memory: 63 MB (12.68%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b) => b - a);
+    s.sort((a,b) => b - a);
+    let i = 0;
+    let j = 0;
+    let res = 0;
+    // g = 
+    while (i < g.length && j < s.length) {
+        if(g[i] <= s[j]) {
+            res++;
+            i++
+            j++;
+        } else {
+            i++;
+        }
+    }
+    return res;
+};
