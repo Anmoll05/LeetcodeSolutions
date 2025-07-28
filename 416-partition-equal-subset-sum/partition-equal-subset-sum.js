bbfd1547744fdbb303101c13a2d7d4dfce695471
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-07-28 20:42:48 +0530

    Time: 344 ms (21.27%) | Memory: 91 MB (32.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -16,27 +16,23 @@
 // };
 
 var canPartition = function(nums) {
+    
     const total = nums.reduce((a, b) => a + b, 0);
     if (total % 2 !== 0) return false;
 
-    const target = total / 2;
-
-    // Initialize 2D memo array: [nums.length][target + 1]
-    const memo = Array.from({ length: nums.length }, () => Array(target + 1).fill(-1));
-
-    const dfs = (i, currSum) => {
-        if (currSum > target) return false;
-        if (currSum === target) return true;
-        if (i >= nums.length) return false;
-
-        if (memo[i][currSum] !== -1) return memo[i][currSum];
-
-        const take = dfs(i + 1, currSum + nums[i]);
-        const skip = dfs(i + 1, currSum);
-
-        memo[i][currSum] = take || skip;
-        return memo[i][currSum];
-    };
-
-    return dfs(0, 0);
+    let half = total / 2;
+    const memo = Array.from({ length: nums.length }, () => Array(half + 1).fill(-1));
+    const dfs = (ind, s) => {
+        if (ind >= nums.length) {
+            if(s == half) {
+                return true;
+            } return false;
+        }
+        if(memo[ind][s] !== -1) return memo[ind][s];
+        let lia = dfs(ind + 1, s + nums[ind]);
+        let niLia = dfs(ind + 1, s);
+        memo[ind][s] = lia || niLia;
+        return lia || niLia;
+    }
+    return dfs(0,0)
 };
