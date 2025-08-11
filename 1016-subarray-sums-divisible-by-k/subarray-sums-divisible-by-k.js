b59613902930512200412983f1a8c4817c9721c4
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-08-11 20:54:54 +0530

    Time: 22 ms (8.60%) | Memory: 61.7 MB (24.89%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
@@ -1,18 +1,34 @@
+const calculateMod = (sum, k) => {
+    return ((sum % k) + k) % k;
+}
 
-const calculatePossitiveModule = (sum, k) => ((sum % k) + k) % k;
-
-var subarraysDivByK = function(nums, k) {
-    let obj = {};
+var subarraysDivByK = function (nums, k) {
     let total = 0;
+    let obj = {};
     let sum = 0;
-    for(let i = 0; i < nums.length; i += 1) {
+    for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
-        const p = calculatePossitiveModule(sum, k);
-        if(p === 0) total += 1;
-        obj[p] = (obj[p] || 0) + 1;
-        if(obj[p] > 1) {
-            total += obj[p] - 1;
+        let p = calculateMod(sum, k);
+        if (p == 0) total += 1;
+
+        if (p in obj) {
+            total += obj[p];
         }
+        obj[p] = ++obj[p] || 1;
     }
     return total;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
