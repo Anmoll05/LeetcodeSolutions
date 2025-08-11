295c019b50858bbb8c0d332e810e94331f605c79
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-08-11 21:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (5.17%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.js
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.js
@@ -1,37 +1,15 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {TreeNode}
- */
 var bstToGst = function(root) {
-    let inorder = [];
-    const dfs = (node) => {
-        if (!node) return;
-        dfs(node.left);
-        inorder.push(node.val);
-        dfs(node.right);
+    let sum = 0;
+
+    const traverse = (node) => {
+        if (node) {
+            traverse(node.right);  // Traverse the right subtree
+            sum += node.val;  // Update the sum
+            node.val = sum;  // Update the current node's value
+            traverse(node.left);  // Traverse the left subtree
+        }
     };
-    dfs(root);
-    let hash = {};
-    let suff = [];
-    for (let i = inorder.length - 1; i >=0; i--) {
-        suff[i] = (suff[i + 1] || 0) + inorder[i];
-        hash[inorder[i]] = suff[i];
-    }
-    //console.log('hash', hash, 'suff', suff, 'inorder', inorder)
-    const dfsA = (node) => {
-        if (!node) return;
-        dfsA(node.left);
-        node.val = hash[node.val]
-        dfsA(node.right);
-    };
-    dfsA(root)
+
+    traverse(root);
     return root;
 };
