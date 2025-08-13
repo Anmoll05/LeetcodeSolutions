5a722afd5f23dcd5ab27375a357f3cb3c7e030fe
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-08-13 20:58:15 +0530

    Time: 1 ms (50.00%) | Memory: 56.1 MB (38.21%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.js
+++ 777-toeplitz-matrix/toeplitz-matrix.js
@@ -3,18 +3,18 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    let hash = {};
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            const k = i - j;
-            if (!(k in hash)) {
-                hash[k] = matrix[i][j]
-            } else {
-                if (matrix[i][j] !== hash[k]) {
-                    return false;
-                }
+    //console.log(matrix[0][0])
+    let res = true;
+    for(let i = 0; i < matrix.length-1; i++) {
+        for (let j = 0; j < matrix[0].length-1; j++) {
+            if(matrix[i][j] !== matrix[i+1][j+1]) {
+                res = false;
+                break;
             }
-        }
+           
+        } if (!res) {
+                break;
+            }
     }
-    return true;
+    return res;
 };
