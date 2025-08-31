290c6c9102737743e4d90fc21994ebd2d12fe7fb
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-08-31 20:41:03 +0530

    Time: 688 ms (15.07%) | Memory: 98.4 MB (5.06%) - LeetSync

--- 127-word-ladder/word-ladder.js
+++ 127-word-ladder/word-ladder.js
@@ -1,33 +1,49 @@
-var ladderLength = function (beginWord, endWord, wordList) {
-    let obj = {};
-    for (let word of wordList) {
-        obj[word] = true;
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    let l = 0;
+    let q = [];
+    let dict = {};
+    for (let w of wordList) {
+        dict[w] = true;
     }
     let vis = {};
+    if (!(endWord in dict)) return 0;
+    q.push(beginWord);
     vis[beginWord] = true;
-    if (!(endWord in obj)) return 0;
-    let q = [];
-    q.push([beginWord, 1]);
-    let res = 0;
     while (q.length) {
-        let [word, l] = q.shift();
-        if (word == endWord) {
-            res = l;
-            break;
-        }
-        for (let i = 0; i < word.length; i++) {
-            let temp = word.split('');
-            for (let ch = 97; ch <= 122; ch++) {
-                temp[i] = String.fromCharCode(ch);
-                let newStr = temp.join('');
-                if (newStr in obj) {
-                    if (!(newStr in vis)) {
-                        vis[newStr] = true;
-                        q.push([newStr, l + 1]);
+        let s = q.length;
+        for (let i = 0; i < s; i++) {
+            let cur = q.shift();
+            if (cur == endWord) {
+                return l + 1;
+            }
+            const curArr = cur.split("");
+            for (let j = 0; j < curArr.length; j++) {
+                const curChar = curArr[j];
+                for (let k = 0; k < 26; k++) {
+                    const replChar = String.fromCharCode(97 + k);
+                    if (replChar !== curChar) {
+                        curArr[j] = replChar;
+                        const key = curArr.join("");
+                        //console.log(key)
+                        if (key in dict && !(key in vis)) {
+                            q.push(key);
+                            vis[key] = true;
+                        }
+
                     }
                 }
+                //console.log('j', j)
+                curArr[j] = cur[j];
             }
         }
+        //console.log('q', q)
+        l++;
     }
-    return (res);
+    return 0;
 };
