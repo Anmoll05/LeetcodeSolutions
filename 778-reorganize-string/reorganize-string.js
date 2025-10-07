247c79cf2ad0fb099b3ae1bbca77b8762736b8ff
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-10-07 21:31:18 +0530

    Time: 20 ms (14.29%) | Memory: 61.4 MB (12.83%) - LeetSync

--- 778-reorganize-string/reorganize-string.js
+++ 778-reorganize-string/reorganize-string.js
@@ -4,27 +4,47 @@
  */
 var reorganizeString = function (str) {
     let freq = {};
-    let res = "";
     for (let s of str) {
         freq[s] = ++freq[s] || 1;
-    };
-    let maxHeap = [...Object.keys(freq)].sort((a, b) => freq[b] - freq[a]);
-    while (maxHeap.length) {
+    }
+    let maxHeap = new PriorityQueue((a, b) => freq[b] - freq[a]);
+    for (let k in freq) {
+        maxHeap.enqueue(k);
+    }
+    let res = '';
+    //console.log(maxHeap.front())
+    while (!maxHeap.isEmpty()) {
+        let q = [];
+        let pop = maxHeap.dequeue();
+        res += pop;
         let i = 0;
-        while (i < 2) {
-            let node = maxHeap.shift();
+        while (i < 1) {
             i++;
-            res += node || "";
-            freq[node] = --freq[node];
-            if (freq[node] > 0) {
-                maxHeap.push(node)
+            if (!maxHeap.isEmpty()) {
+                let poppedSecond = maxHeap.dequeue();
+                res += poppedSecond;
+                freq[poppedSecond] = freq[poppedSecond] - 1;
+                if (freq[poppedSecond] <= 0) {
+                    delete freq[poppedSecond];
+                }
+                else {
+                    q.push(poppedSecond);
+                }
             }
         }
-        maxHeap.sort((a, b) => freq[b] - freq[a]);
+        freq[pop] = freq[pop] - 1;
+        if (freq[pop] <= 0) {
+            delete freq[pop];
+        }
+        else {
+            q.push(pop);
+        }
+        while (q.length) {
+            maxHeap.enqueue(q.pop());
+        }
     }
-    //console.log('res', res)
-    for (let i = 0; i < res.length - 1; i++) {
-        if (res[i] == res[i + 1]) {
+    for (let i = 1; i < res.length; i++) {
+        if(res[i] == res[i - 1]) {
             return "";
         }
     }
