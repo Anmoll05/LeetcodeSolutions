cfeb702a0da8f4e0e4da0c5da695fd0ceac638da
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-10-21 20:37:20 +0530

    Time: 151 ms (91.30%) | Memory: 64.8 MB (95.65%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.js
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.js
@@ -5,21 +5,48 @@
  * @param {number} right
  * @return {number}
  */
-var rangeSum = function(nums, n, left, right) {
-   const result = [];
-   const mod = 1000000007;
-  for (let i = 0; i < nums.length; i++) {
-    let sum = 0; // running sum from i
-    for (let j = i; j < nums.length; j++) {
-      sum += nums[j];
-      result.push(sum);
-    }
-  }
+var rangeSum = function (nums, n, left, right) {
+    // const result = [];
+    // const mod = 1000000007;
+    // for (let i = 0; i < nums.length; i++) {
+    //     let sum = 0; // running sum from i
+    //     for (let j = i; j < nums.length; j++) {
+    //         sum += nums[j];
+    //         result.push(sum);
+    //     }
+    // }
 
-  result.sort((a,b) => a - b);
-  let res = 0;
-  for (let i = left - 1; i <= right - 1; i++) {
-    res = (res  + result[i] ) % mod
-  }
-  return res
+    // result.sort((a, b) => a - b);
+    // let res = 0;
+    // for (let i = left - 1; i <= right - 1; i++) {
+    //     res = (res + result[i]) % mod
+    // }
+    // return res
+    const mod = 1000000007;
+    let pq = new PriorityQueue((a, b) => {
+        if (a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0]; // min-heap by sum
+    });
+
+    // push all single-element subarrays
+    for (let i = 0; i < n; i++) {
+        pq.enqueue([nums[i], i]); // [currentSum, endIndex]
+    }
+
+    let s = 0;
+    // pop 'right' smallest sums; add those in range [left, right]
+    for (let cnt = 1; cnt <= right; cnt++) {
+        const [curSum, endIdx] = pq.dequeue();
+
+        if (cnt >= left) {
+            s = (s + curSum) % mod;
+        }
+
+        const nextIdx = endIdx + 1;
+        if (nextIdx < n) {
+            pq.enqueue([curSum + nums[nextIdx], nextIdx]);
+        }
+    }
+
+    return s;
 };
