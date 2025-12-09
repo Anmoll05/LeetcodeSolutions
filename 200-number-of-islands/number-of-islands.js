f7a8156ce098a169001e61a32dce6dbbb9130ea7
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-12-09 17:05:05 +0530

    Time: 652 ms (5.07%) | Memory: 66.2 MB (20.57%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -3,25 +3,24 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let cnt = 0;
     const dfs = (i,j) => {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
-            return;
-        }
+        console.log(i,j)
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == "0") return;
         grid[i][j] = '0';
         dfs(i + 1, j);
         dfs(i - 1, j);
-        dfs(i, j + 1);
+        dfs(i,j + 1);
         dfs(i, j - 1);
         return;
     };
-    for (let i = 0; i < grid.length; i++) {
+    let ct = 0;
+    for(let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if(grid[i][j] == '1') {
+            if(grid[i][j] == "1") {
                 dfs(i,j);
-                cnt++;
+                ct++;
             }
         }
     }
-    return cnt;
+    return ct;
 };
