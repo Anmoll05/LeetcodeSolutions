c663d12e02086dbcf271d5d6fa6bf04252b9b27a
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2025-12-11 23:55:56 +0530

    Time: 108 ms (5.68%) | Memory: 64.7 MB (5.30%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function (root, target, k) {
+    let res = [];
+    let vis = {};
+    const makeResult = (node, l) => {
+        if (!node) return;
+        if(vis[node.val]) return;
+        if (l > k) return;
+        vis[node.val] = true;
+        if (l == k) res.push(node.val);
+        makeResult(node.left, l + 1);
+        makeResult(node.right, l + 1);
+        makeResult(myParent[node.val.toString()], l + 1);
+    }
+    let myParent = {};
+    const dfs = (node, prev) => {
+        if (!node) return;
+        myParent[node.val] = prev;
+        //console.log(node.val)
+        if (node.val == target.val) {
+            //console.log(node.val)
+            makeResult(node, 0);
+            return;
+        }
+        dfs(node.left, node);
+        dfs(node.right, node);
+    }
+    dfs(root, null);
+    console.log(myParent)
+    return res
+};
