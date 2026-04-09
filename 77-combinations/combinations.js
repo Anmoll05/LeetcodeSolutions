70fd14f5ed5d60d720af59574f71c9504da91c5c
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2026-04-09 23:13:47 +0530

    Time: 159 ms (5.31%) | Memory: 157.4 MB (5.10%) - LeetSync

---
+++ 77-combinations/combinations.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    let res = [];
+    const backtrack = (ind,arr) => {
+        if (ind > n + 1) return;
+        
+        if(arr.length == k) {
+            res.push(arr);
+            return;
+        }
+        backtrack(ind + 1, [...arr,ind]);
+        backtrack(ind + 1, arr);
+    };
+    backtrack(1,[]);
+    return res;
+};
