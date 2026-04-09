fee4677b67b245f0eca5657de52a614a819169cc
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2026-04-09 23:36:10 +0530

    Time: 6 ms (25.28%) | Memory: 68.6 MB (5.37%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+    let res = [];
+    const dfs = (node, temp, prevSum) => {
+        if (!node) return;
+        if (!node.left && !node.right && (prevSum + node.val) == targetSum) {
+            res.push([...temp, node.val]);
+            return;
+        }
+        dfs(node.left, [...temp, node.val], prevSum + node.val);
+         dfs(node.right, [...temp, node.val], prevSum + node.val);
+    };
+    dfs(root,[], 0);
+    return res;
+};
