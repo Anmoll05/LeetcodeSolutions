3d13f20ff03403a59102ceaf0e82148c150b5097
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2026-04-11 23:23:49 +0530

    Time: 312 ms (5.06%) | Memory: 102.5 MB (5.09%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,28 +3,51 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums.length === 0) return 0;
-    const explore = (e) => {
-        if(e.toString() in isPresent) {
-            covered[e] = true;
-            if (e.toString() in memo) {
-                //console.log('in memo', memo[e])
-                return memo[e];
-            }
-           // console.log('e :',e , explore(e-1) )
-            memo[e] = 1 + explore(parseInt(e-1)) ;
-            return memo[e];
+    if (nums.length===0) return 0;
+    let parent = {};
+    let size = {};
+    const UnionFind = function(nums) {
+        nums.forEach((e) => {
+            parent[e] = e;
+            size[e] = 1;
+        });
+
+        this.find = (n) => {
+            if(parent[n] == n) return n;
+            parent[n] = this.find(parent[n]);
+            return parent[n];
         }
-        return 0;
+
+        this.union = (m,n) => {
+            const rootM = this.find(m);
+            const rootN = this.find(n);
+
+            const rankM = size[rootM];
+            const rankN = size[rootN];
+
+            if (rootM == rootN) return;
+            if (rankM < rankN) {
+                size[rootN] += size[rootM];
+                parent[rootM] = rootN;
+            } else {
+                size[rootM] += size[rootN];
+                parent[rootN] = rootM;
+            }
+        }
     }
-    let isPresent = {};
-    nums.map((e) => {
-        isPresent[e] = 1;
-    });
-    var memo = {};
-    var covered = {};
-    nums.map((e) => {
-        if (!(e in covered)) explore(e);
-    });
-    return Math.max(...(Object.values(memo)));
+
+    const uf = new UnionFind(nums);
+
+    for (let num of nums) {
+        if ((num - 1) in parent) {
+            uf.union(num - 1, num);
+        }
+    }
+
+    let max = 0;
+    for (let num of nums){
+        max = Math.max(max, size[num]);
+    }
+
+    return max;
 };
