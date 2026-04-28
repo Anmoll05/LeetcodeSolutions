3c75a0275d3e2b805804bdc639771815aaea8028
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2026-04-28 22:13:51 +0530

    Time: 3 ms (76.92%) | Memory: 57.1 MB (53.85%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.js
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.js
@@ -1,31 +1,14 @@
 var secondsToRemoveOccurrences = function (s) {
-    const isStringValid = (arr) => {
-        let foundZero = false;
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === '0') {
-                foundZero = true;
-            }
-            if (foundZero && arr[i] === '1') return false;
+    let zeros = 0;
+    let time = 0;
+
+    for (let ch of s) {
+        if (ch === '0') {
+            zeros++;
+        } else if (zeros > 0) {
+            time = Math.max(time + 1, zeros);
         }
-        return true;
-    };
-
-    let cs = s.split(''); // ✅ array now
-    let c = 0;
-
-    while (!isStringValid(cs)) {
-        // console.log(cs.join('')); // optional debug
-
-        for (let i = 0; i < cs.length - 1; i++) {
-            if (cs[i] === '0' && cs[i + 1] === '1') {
-                // swap
-                cs[i] = '1';
-                cs[i + 1] = '0';
-                i++; // skip next
-            }
-        }
-        c++;
     }
 
-    return c;
+    return time;
 };
