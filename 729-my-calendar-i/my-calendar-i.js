4822211ea6966d3d44c9f6d5e84b2e80e7d84ab5
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2026-08-08 15:55:27 +0530

    Time: 186 ms (5.34%) | Memory: 66.4 MB (5.34%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.js
@@ -0,0 +1,29 @@
+
+var MyCalendar = function() {
+    this.list = [];
+};
+
+/** 
+ * @param {number} startTime 
+ * @param {number} endTime
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function(startTime, endTime) {
+    //console.log(this.list)
+    for (let i = 0; i < this.list.length; i++) {
+        const [st, end] = this.list[i];
+        if ((startTime < end && endTime > st) || (startTime >= st && endTime < end) || 
+        (startTime <= st && endTime > st)) {
+            return false;
+        }
+    }
+    this.list.push([startTime, endTime]);
+    this.list.sort((a,b) => a[0] - b[0]);
+    return true;
+};
+
+/** 
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(startTime,endTime)
+ */
