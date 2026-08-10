3a55eecb6beb132d4177839c5bca5ad9b878b6e0
Anmoll05 <44276785+Anmoll05@users.noreply.github.com>
2026-08-11 01:23:34 +0530

    Time: 101 ms (0.00%) | Memory: 65 MB (11.11%) - LeetSync

---
+++ 250-count-univalue-subtrees/count-univalue-subtrees.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countUnivalSubtrees = function(root) {
+    
+    let res = 0;
+    const dfs = (node) => {
+        if(!node) return {val: -1001, is: true};
+        let l = dfs(node.left);
+        let r = dfs(node.right);
+        console.log(l,r, l.val  == -1001)
+        if (l.val == r.val && r.val == -1001) {
+            res++;
+            return {val: node.val, is: true}
+        }
+        if ((l.val == r.val && r.val == node.val && l.is && r.is)) {
+            res++;
+            return {val: node.val, is: true}
+        }
+        if (l.val == node.val && (r.val == -1001) && l.is) {
+             res++;
+            return {val: node.val, is: true}
+        }
+         if (l.val== -1001 && (r.val == node.val) && r.is) {
+             res++;
+            return {val: node.val, is: true}
+        }
+        return {val: node.val, is: false};
+    }
+    dfs(root);
+    return res;
+};
